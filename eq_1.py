cf3bbfc40a0fe65122706b58a4cefb72f10608ee
Asaolu <Akintoye_Asaolu1@baylor.edu>
2020-02-03 16:51:24 -0600

    Class Work feb_3_2020

--- eq_1.py
+++ eq_1.py
@@ -22,7 +22,7 @@ for eq in list_of_eqs:
     mag = eq['properties']['mag']
     lon= eq['geometry']['coordinates'][0]
     lat= eq['geometry']['coordinates'][1]
-    mags.append (mags)
+    mags.append (mag)
     lons.append(lon)
     lats.append(lat)
 
